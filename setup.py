a42aa4c7375a8755b438de444f4e8e85daae24a7
Youngmin Kim <ymkim92@gmail.com>
2023-05-06 20:42:11 +1000

    update version to 0.2

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-setup(name='fuel_price',
-    scripts=['bin/racq_fuel_price'],
-    version='0.1',
-    description='Fuel price in Queensland, Australia',
-    author='Youngmin Kim',
-    author_email='ymkim92@gmail.com',
-    license='WTFPL',
+setup(
+    name="fuel_price",
+    scripts=["bin/racq_fuel_price"],
+    version="0.2",
+    description="Fuel price in Queensland, Australia",
+    author="Youngmin Kim",
+    author_email="ymkim92@gmail.com",
+    license="WTFPL",
     packages=find_packages(),
     install_requires=[
-        'numpy',
-        'requests',
+        "numpy",
+        "requests",
     ],
 )
